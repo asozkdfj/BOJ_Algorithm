d9f73351d925f9c65901f0c2fb788bec9991af84
asozkdfj <tfy203@gmail.com>
2023-03-16 04:21:56 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+//1026보물
+int n;
+int a[51];
+int b[51];
+int ind[51];
+int answer[51];
+priority_queue<int> pq;
+bool cmp(const int& a, const int& b) {
+    if (a == b) {
+        return false;
+    }
+    return a < b;
+}
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) { cin >> a[i]; }
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+        pq.push(b[i]);
+    }
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (pq.top() == b[j] && (!ind[j])) {
+                cnt++;
+                ind[j] = cnt;
+                pq.pop();
+                break;
+            }
+        }
+    }
+
+    sort(a, a + n, cmp);
+    //s계산
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        int in = ind[i] - 1;
+        ans+= a[in] * b[i];
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
